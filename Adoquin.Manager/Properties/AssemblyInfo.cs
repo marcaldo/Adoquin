0aa1dd2a56aba354ba254ebf303f4b00ca412422
marcaldo <marcaldo@gmail.com>
2015-08-28 14:47:14 -0300

    Architecture

--- Adoquin.Manager/Properties/AssemblyInfo.cs
+++ Adoquin.Manager/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Adoquin.AccountManager")]
+[assembly: AssemblyTitle("Adoquin.Manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Adoquin.AccountManager")]
+[assembly: AssemblyProduct("Adoquin.Manager")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dd4fd532-600a-40ca-ac73-520feb438b34")]
+[assembly: Guid("f42c29f6-7a03-4e29-855b-a18bc4b69ae5")]
 
 // Version information for an assembly consists of the following four values:
 //
